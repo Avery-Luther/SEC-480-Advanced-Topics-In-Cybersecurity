0d40ba07fdb3feca4391e11c990e5a8e970e2459
avery-luther <avery.luther@mymail.champlain.edu>
2026-04-21 10:58:54 -0400

    milestone 7 initial commit

---
+++ milestone7/pwsh/rockyClones.ps1
@@ -0,0 +1,9 @@
+$480Path = Join-Path $PSScriptRoot "../../480Utils.psm1"
+Import-Module $480Path
+
+Connect-480Server('vcenter.avery.loval')
+$RockyConfigPath = Join-Path $PSScriptRoot "./cloneRocky.json"
+for ($i=0; $i -lt 3; $i++){
+	New-480Clone($RockyConfigPath)
+	Set-480Network($RockyConfigPath)
+}
